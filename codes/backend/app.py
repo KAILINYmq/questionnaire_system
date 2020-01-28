3ccd0c98ea573f7a7ed1b5b015349a94ea7792ac
myuanz <provefars@gmail.com>
2020-01-28 23:01:20 +0800

    修改jinja界定符

--- codes/backend/app.py
+++ codes/backend/app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 from views.testflask import view
 app.register_blueprint(view, url_prefix='/test')
 
+app.jinja_env.block_start_string = '(%' # 修改块开始符号
+app.jinja_env.block_end_string = '%)' # 修改块结束符号
+app.jinja_env.variable_start_string = '((' # 修改变量开始符号
+app.jinja_env.variable_end_string = '))' # 修改变量结束符号
+app.jinja_env.comment_start_string = '(#' # 修改注释开始符号
+app.jinja_env.comment_end_string = '#)' # 修改注释结束符号
+
 if __name__ == "__main__":
     app.run(debug=True, port=80)
